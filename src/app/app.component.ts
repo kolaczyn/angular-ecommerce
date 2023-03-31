3cd43a521291701fc98c6038950eb16c94e5aa68
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-03-31 16:42:27 +0200

    show products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { ProductsRespose } from 'src/types';
+
+const PRODUCTS_URL =
+  'https://v5stg.rossmann.pl/products/v2/api/Products?ShopNumber=735&PageSize=15&Page=1';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'product-filters';
+  counter = 0;
+
+  constructor(private http: HttpClient) {}
+
+  products$ = this.http
+    .get<ProductsRespose>(PRODUCTS_URL)
+    .pipe(map((x) => x.data.products));
+
+  increment() {
+    this.counter++;
+  }
 }
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+type ProductDto = {
+  brand: string;
+  caption: string;
+  navigateUrl: string;
+};
+
+export type ProductsRespose = {
+  data: {
+    products: ProductDto[];
+    totalPages: number;
+    totalCount: number;
+  };
+};
