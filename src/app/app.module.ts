cafc8259f2d7eeee97ea353d404acf3af3f974aa
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-04-03 09:42:31 +0200

    create navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductTileComponent } from './components/product-tile/product-tile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FiltersComponent } from './filters/filters.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductTileComponent, FiltersComponent],
+  declarations: [
+    AppComponent,
+    ProductTileComponent,
+    FiltersComponent,
+    LayoutComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -21,6 +28,7 @@ import { FiltersComponent } from './filters/filters.component';
     MatButtonModule,
     MatCardModule,
     MatSelectModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
